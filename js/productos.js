361d91748b7a4498137e1e86253562e2f24675dd
clarigullon <clariigullon@gmail.com>
2021-09-04 12:57:11 -0300

    terminando carrito de compras

--- js/productos.js
+++ js/productos.js
@@ -10,112 +10,112 @@ class Producto {
   }
   const superdesayuno = new Producto(
     "Super Desayuno",
-    Math.random().toString(36).substr(2, 9),
+    1,
     1800,
     "imagenesventa/desayunog-min.jpg",
     0
   );
   const desayuno = new Producto(
     "Desayuno",
-    Math.random().toString(36).substr(2, 9),
+    2,
     1500,
     "imagenesventa/desayunochic-min.jpg",
     0
   );
   const bandejitagrande = new Producto(
     "Bandejita de tortitas x16",
-    Math.random().toString(36).substr(2, 9),
+    3,
     900,
     "imagenesventa/bandejitagrande-min.jpg",
     0
   );
   const cabsha = new Producto(
     "Torta Cabsha",
-    Math.random().toString(36).substr(2, 9),
+    4,
     1200,
     "imagenesventa/cabsha-min.jpeg",
     0
   );
   const rogel = new Producto(
     "Rogel",
-    Math.random().toString(36).substr(2, 9),
+    5,
     1300,
     "imagenesventa/rogel2.JPG",
     0
   );
   const helada = new Producto(
     "Torta Helada",
-    Math.random().toString(36).substr(2, 9),
+    6,
     1200,
     "imagenesventa/helad-min.JPG",
     0
   );
   const brownie = new Producto(
     "Brownie",
-    Math.random().toString(36).substr(2, 9),
+    7,
     1300,
     "imagenespilares/postreddl.jpg",
     0
   );
   const bombaf = new Producto(
     "Torta Rellena",
-    Math.random().toString(36).substr(2, 9),
+    8,
     1300,
     "imagenesventa/bombafrut-min.jpg",
     0
   );
   const frutilla = new Producto(
     "Tarta de Frutilla",
-    Math.random().toString(36).substr(2, 9),
+   9,
     900,
     "imagenesventa/tartafrut.jpg",
     0
   );
   const lemonpie = new Producto(
     "Lemon Pie",
-    Math.random().toString(36).substr(2, 9),
+    10,
     900,
     "imagenesventa/lemonpie-min.jpg",
     0
   );
   const alfachoco = new Producto(
     "Alfajores de Chocolate",
-    Math.random().toString(36).substr(2, 9),
+    11,
     500,
     "imagenesventa/alfachocolate-min.JPG",
     0
   );
   const maizena = new Producto(
     "Alfajorcitos de Maizena",
-    Math.random().toString(36).substr(2, 9),
+    12,
     500,
     "imagenesventa/maizenavert.jpg",
     0
   );
   const nuez = new Producto(
     "Alfajorcitos de Nuez",
-    Math.random().toString(36).substr(2, 9),
+    13,
     500,
     "imagenesventa/alfanuez-min.jpg",
     0
   );
   const alfajocookies = new Producto(
     "Alfacookies",
-    Math.random().toString(36).substr(2, 9),
+    14,
     600,
     "imagenesventa/paraelte.jpg",
     0
   );
   const chips = new Producto(
     "Cookies con Chips",
-    Math.random().toString(36).substr(2, 9),
+    15,
     250,
     "imagenesventa/cookieschoco-min.jpg",
     0
   );
   const rellenas = new Producto(
     "Cookies rellenas de limón",
-    Math.random().toString(36).substr(2, 9),
+    16,
     300,
     "imagenesventa/havanna-min.jpg",
     0
@@ -123,77 +123,77 @@ class Producto {
    
   const limon = new Producto(
     "Cookies de limón",
-    Math.random().toString(36).substr(2, 9),
+    17,
     250,
     "imagenesventa/limon-min.jpg",
     0
   );
   const anillos = new Producto(
     "Anillos bañados en chocolate",
-    Math.random().toString(36).substr(2, 9),
+    18,
     300,
     "imagenesventa/anillos-min.JPG",
     0
   );
   const pandecampo = new Producto(
     "Pan de Campo",
-    Math.random().toString(36).substr(2, 9),
+    19,
     350,
     "imagenesventa/pancampo.jpg",
     0
   );
   const pandemolde = new Producto(
     "Pan de Molde",
-    Math.random().toString(36).substr(2, 9),
+    20,
     300,
     "imagenesventa/pandemolde.jpg",
     0
   );
   const pandetrigo = new Producto(
     "Pan de Trigo Sarraceno",
-    Math.random().toString(36).substr(2, 9),
+    21,
     300,
     "imagenesventa/pantrigosarra.jpg",
     0
   );
   const pandequeso = new Producto(
     "Pancitos de Queso",
-    Math.random().toString(36).substr(2, 9),
+    22,
     440,
     "imagenesventa/panqueso.jpg",
     0
   );
   const pancitotrigo = new Producto(
     "Pancitos de Trigo Sarraceno",
-    Math.random().toString(36).substr(2, 9),
+    23,
     1100,
     "imagenesventa/pancitostrigo.jpg",
     0
   );
   const panchips = new Producto(
     "Pancitos de Chips",
-    Math.random().toString(36).substr(2, 9),
+    24,
     280,
     "imagenesventa/chips-min.jpg",
     0
   );
   const pizza = new Producto(
     "Prepizza",
-    Math.random().toString(36).substr(2, 9),
+    25,
     110,
     "imagenesventa/pizza.jpg",
     0
   );
   const bizcochitosgrasa = new Producto(
     "Bizcochitos de grasa",
-    Math.random().toString(36).substr(2, 9),
+    26,
     250,
     "imagenesventa/bizcochitosgrasa-min.JPG",
     0
   );
   const bizcochitoslibritos = new Producto(
     "Bizcochitos Libritos",
-    Math.random().toString(36).substr(2, 9),
+    27,
     300,
     "imagenesventa/bizcochitoslibritos-min.JPG",
     0
@@ -230,24 +230,24 @@ bizcochitoslibritos
 );
 let paginaActual = window.location.pathname;
 window.onload = function (){
-    $("#mySidenav").append( `<div class="totalCompra"><h3>TOTAL:</h3>
-    <input id="totalCompra" value=0 type="text">
-    </div>`)
-
+    $("#totalCompras").append( `<h3>TOTAL:</h3>
+    <input id="totalCompra" value=0 type="text">`)
+    deshabilitarBotones();
 }
 for (const producto of productos) {
     $("#postresCards")
         .append(`<div class="productosCarrito" id=${producto.id}> <img src= ${producto.img} />
-                <p>  ${producto.nombre}</p>
-                <b> $ ${producto.precio}</b> <br>
-                <button id="${producto.id}" class="btn">AGREGAR</button> <br>
+                <p> ${producto.nombre}</p>
+                <b> $${producto.precio}</b> <br>
+                <button id="agregar${producto.id}" class="btn-agregar">AGREGAR</button> <br>
                 </div> `);
 }
-$(".btn").click((e)=>agregarProducto(e))
+$(".btn-agregar").click((e)=>agregarProducto(e))
 function agregarClick(e) {
 if ((e.target !== null) && (e.target !== "undefined")) {
     e.target.style.opacity = "0.8";
     e.target.style.backgroundColor = "#ffd7ba"; 
+    e.target.disabled = true;
     e.target.innerHTML = "Agregado!"; 
     } 
 }
@@ -255,7 +255,8 @@ let carrito = [];
 let carritoSeccion = []
 function agregarProducto(e) {
   agregarClick(e);
-  let productoClickeado = productos.find((item) => item.id == e.target.id);
+  let id = e.target.id.split("agregar")[1]
+  let productoClickeado = productos.find((item) => item.id == id);
   if (localStorage.getItem("MiCarrito") !== null) {
     carrito = JSON.parse(localStorage.getItem("MiCarrito"))
   }
@@ -269,6 +270,8 @@ function agregarProducto(e) {
   localStorage.setItem(productoClickeado.seccion, JSON.stringify(carritoSeccion));
   agregarItem(productoClickeado);
   total(productoClickeado);
+  document.getElementById("totalCompras").style.visibility = "visible";
+  document.getElementById("carritoVacio").style.visibility = "hidden";
 }
 function total(productoClickeado){
   let totalActual = $("#totalCompra").val()
@@ -279,9 +282,9 @@ function agregarItem(producto) {
   $("#mySidenav").append(` <div class="itemCarrito" id="remover${producto.id}"><h3>${producto.nombre}</h3>
   <img src= ${producto.img} />
   <b>$${producto.precio}</b> 
-  <div type="button"><i class="fas fa-plus sumarIcon" id="${producto.id}"></i></div>
+  <div type="button"><i class="fas fa-chevron-up sumarIcon" id="${producto.id}"></i></div>
+  <div type="button"><i class="fas fa-chevron-down restarIcon" id="${producto.id}"></i></div>
   <input id="contador${producto.id}" class="inputCantidad" value="${cantidadItem(producto.id)}" type="text">
-  <div type="button"><i class="fas fa-window-minimize restarIcon" id="${producto.id}"></i></div>
   <div type="button"><i class="fas fa-trash btn-remover" id="${producto.id}"></i></div>
   </div>`);
   $(".btn-remover").click((e)=>removerItem(e))
@@ -292,7 +295,6 @@ function cantidadItem (e) {
   carrito=JSON.parse(localStorage.getItem("MiCarrito"));
   if (carrito != null) {
     let item = carrito.find((item) =>item.id==e);
-    console.log(e)
     let cant = item.cantidad;
     return cant;
   }
@@ -314,12 +316,14 @@ function restarItem (e) {
   carrito = JSON.parse(localStorage.getItem("MiCarrito"));
   let valorActual = parseInt($("#totalCompra").val())
   let item = carrito.find((item) => item.id == e.target.id);
-  let cantidad = parseInt($("#contador"+ item.id).val()) -1;
-  $("#contador"+item.id).val(cantidad);
-  valorActual -= item.precio;
-  item.cantidad -= 1;
-  $("#totalCompra").val(parseInt(valorActual));
-  localStorage.setItem("MiCarrito", JSON.stringify(carrito));
+  if (parseInt($("#contador"+ item.id).val()) >= 2) {
+    let cantidad = parseInt($("#contador"+ item.id).val()) -1;
+    $("#contador"+item.id).val(cantidad);
+    valorActual -= item.precio;
+    item.cantidad -= 1;
+    $("#totalCompra").val(parseInt(valorActual));
+    localStorage.setItem("MiCarrito", JSON.stringify(carrito));
+  }
 }
 function mostrarItems(array) {
   for (const producto of array) {
@@ -338,7 +342,14 @@ function removerItem(e) {
     localStorage.setItem("MiCarrito", JSON.stringify(carrito));
     let idARemover = "#remover" + e.target.id
     $(idARemover).remove()
-    $("#totalCompra").val(valorActual)
+    $("#totalCompra").val(valorActual);
+    let productoClickeado = productos.find((item) => item.id == e.target.id);
+    productoClickeado.cantidad = 0;
+    rehabilitarBoton(item);
+    if (valorActual == 0) {
+        document.getElementById("totalCompras").style.visibility = "hidden";
+        document.getElementById("carritoVacio").style.visibility = "visible";
+    }
   }
 }
 $("#sideNavClose").click(() => closeNav())
@@ -347,14 +358,23 @@ function closeNav() {
 }
 $("#carrito").click(() => openNav())
 function openNav() {
+  let carritoLocalStorage = JSON.parse(localStorage.getItem("MiCarrito"))
   document.getElementById("mySidenav").style.width = "250px";
-  if (localStorage.getItem("MiCarrito") != null) {
-    let carritoLocalStorage = JSON.parse(localStorage.getItem("MiCarrito"))
-    console.log(document.getElementById("mySidenav").childElementCount)
+  if (carritoLocalStorage == null || carritoLocalStorage === [] || (Array.isArray(carritoLocalStorage) && carritoLocalStorage.length == 0)) {
+    console.log("aca estoy");
+    document.getElementById("carritoVacio").style.visibility = "visible";
+    document.getElementById("totalCompras").style.visibility = "hidden";
+  }
+  else {
+    console.log("entre tmb")
+    document.getElementById("carritoVacio").style.visibility = "hidden";
+    document.getElementById("totalCompras").style.visibility = "visible";
+   
     if (document.getElementById("mySidenav") !== null && document.getElementById("mySidenav").childElementCount <= 3) {
         mostrarItems(carritoLocalStorage);
       }
-    } totalGeneral ()
+   } 
+   totalGeneral ()
 }
 function totalGeneral (){
     if (JSON.parse(localStorage.getItem("MiCarrito") != null)) {
@@ -364,25 +384,28 @@ function totalGeneral (){
             carrito.forEach(producto => {
                 precioFinal += producto.precio * producto.cantidad
             });
-            console.log(precioFinal)
             $("#totalCompra").val(precioFinal)
         }
     }
   }
-function eliminarProductoLocalStorage(carrito){
-    let productosLS;
-    productosLS = this.producto();
-    productosLS.forEach(function(productoLS, index){
-        if(productoLS.id === carrito){
-            productosLS.splice(index, 1);
-        }
-    });
-  
-    localStorage.setItem('MiCarrito', JSON.stringify(carrito));
-  }
-  function vaciarLocalStorage(){
-    localStorage.clear();
-  }
-  function getSeccion() {
-    return "producto"
-  }
+function deshabilitarBotones() {
+    if (JSON.parse(localStorage.getItem("MiCarrito")) != null) {
+        carrito = JSON.parse(localStorage.getItem("MiCarrito"));
+        carrito.forEach(producto => {
+            let button =  $("#agregar"+producto.id).get()[0];
+            if (button != null || typeof(button) !== "undefined"){
+                button.disabled = true;
+                button.style.opacity = "0.8";
+                button.style.backgroundColor = "#ffd7ba"; 
+                button.innerHTML = "Agregado!"; 
+            }
+        })
+    }
+}
+
+function rehabilitarBoton(producto) {
+    let button = $("#agregar"+producto.id).get()[0];
+    button.disabled = false;
+    button.style.backgroundColor = "#f0efeb"; 
+    button.innerHTML = "AGREGAR"; 
+}
